c17960d80471410049f3f1bc7ccca88a4561443a
Joice Aurino Lima <joice.aurino@gmail.com>
2025-07-21 22:24:57 -0300

    chore: fix groupId

--- src/main/java/com/usekeepwise/backend/inventory/InventoryApplication.java
+++ src/main/java/com/usekeepwise/backend/inventory/InventoryApplication.java
@@ -1,4 +1,4 @@
-package com.usekeepwise.backend.inventory.inventory;
+package com.usekeepwise.backend.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
